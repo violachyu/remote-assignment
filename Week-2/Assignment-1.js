d0ec5bb1336ca3cf5f4bcbbbc0278a091a87e74f
Viola Chyu <37212869+violachyu@users.noreply.github.com>
2020-05-01 19:48:37 +0800

    Add files via upload

--- Week-2/Assignment-1.js
+++ Week-2/Assignment-1.js
@@ -1,24 +1,29 @@
+// find max
 function max(numbers) {
-    for (i = 0; i <= numbers.length; i++) {
-        var base = numbers[0];
+    var base = numbers[0];
+    for (i = 1; i < numbers.length; i++) {
         if (numbers[i] > base) {
-            var base = numbers[i];
-        };
-        return base;
+            base = numbers[i];
+        }
     }
-}
+    return base;
+};
+
+
 console.log(max([1, 2, 4, 5]));
 console.log(max([5, 2, 7, 1, 6]));
 
+// findPosition
 function findPosition(numbers, target) {
     for (i = 0; i <= numbers.length; i++) {
         if (target == numbers[i]) {
             return i;
+        } else if (target !== numbers[i] && i == numbers.length) {
+            return -1;
         }
-        //if (target )
     }
 }
-console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0 
+console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
 console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
 console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7)); // should print 2 (the first position) 
 console.log(findPosition([5, 2, 7, 1, 6], 8)); // should print -1
