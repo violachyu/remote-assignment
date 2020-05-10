a533de928cbe89dedc46dc4af4e8f00ac0431596
Viola Chyu <37212869+violachyu@users.noreply.github.com>
2020-05-11 05:20:22 +0800

    Add files via upload

--- Week-3/app.js
+++ Week-3/app.js
@@ -1,14 +1,48 @@
 
 const express = require("express");
 const app = express();
+app.use('/sum.html', express.static('static'));
 
-const mainRoutes = require('./routes/index');
-// const getDataRoutes = require('./routes/getData');
+const cookieParser = require('cookie-parser');
 
-app.use('/', mainRoutes);
-// app.use('/getData', getDataRoutes);
+app.use(cookieParser());
+app.set('view engine', 'pug');
 
-// app.set('view engine', 'pug');
+// Assignment-1
+app.get('/', (req, res) => {
+    res.send('Hello, My Server!');
+})
+// Assignment-2
+app.get('/getData', (req, res) => {
+    const number = req.query.number;
+    if (number === 'xyz') {
+        res.send("Wrong Parameter");
+    } else if (!number) {
+        res.send('Lack of Parameter');
+    } else {
+        let sum = 0;
+        for (var i = 1; i <= parseInt(number); i++) {
+            sum += i;
+        }
+        res.send(`<h1>${sum}</h1>`);
+    }
+
+}
+);
+
+//Assignment-4
+app.get('/myName', (req, res) => {
+    res.render('myName', { name: req.cookies.username });
+})
+
+app.post('/myName', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.render('myName', { name: req.body.username });
+})
+
+app.get('/trackName', (req, res) => {
+
+})
 
 app.listen(3000, () => {
     console.log("App is running!");
